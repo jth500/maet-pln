fe41e902cc0f90257b736d1a39686585a529d1cd
louise83339 <160757894+louise83339@users.noreply.github.com>
2024-03-05 13:55:29 +0000

    add loss functions

--- src/loss.py
+++ src/loss.py
@@ -1,3 +1,8 @@
+!pip install rouge-score
+from rouge_score import rouge_scorer
+
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'],
+                                  use_stemmer=True)
 def rouge(y_true: str, y_pred: str) -> float:
     """Rouge loss function to be applied element wise.
 
@@ -8,11 +13,41 @@ def rouge(y_true: str, y_pred: str) -> float:
     Returns:
         float: Loss value.
     """
-    # TODO: Implement rouge loss
-    # Not sure if inputs should be strings or tokenised lists?? Haven't looked at this at all
-    # presumably there's a library for this?
-    return None
+    scores = scorer.score(y_true, y_pred)
 
+    precision = [scores['rouge1'].precision, scores['rouge2'].precision, scores['rougeL'].precision]
+    recall = [scores['rouge1'].recall, scores['rouge2'].recall, scores['rougeL'].recall]
+    f1_score = [scores['rouge1'].fmeasure, scores['rouge2'].fmeasure, scores['rougeL'].fmeasure]
+
+    return precision, recall, f1_score
+#currently has three version of rouge evaluation metrics
+#would probably use rougeL
+
+y_true = "my name is Louise "
+y_pred = "is my name Louise"
+
+out = rouge(y_true, y_pred)
+print(out)
+
+!pip install transformers
+!pip install bert-score
+
+from transformers import BertTokenizer, BertForMaskedLM, BertModel
+from bert_score import BERTScorer
+
+tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+model = BertModel.from_pretrained("bert-base-uncased")
+
+def BERTScore(y_true: str, y_pred: str) -> float:
+
+  # prepare texts for BERT
+  scorer = BERTScorer(model_type='bert-base-uncased')
+  P, R, F1 = scorer.score([y_true], [y_pred])
+
+  precision = round(P.mean().item(),4)
+  recall = round(R.mean().item(),4)
+  F1 = round(F1.mean().item(),4)
+  return precision, recall, F1
 
 def other_loss_metric(y_true: str, y_pred: str) -> float:
     # TODO
