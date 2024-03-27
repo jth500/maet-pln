69c5e1a3fd925c830ebb629c6375100d34c87703
Lucia Guillamet <lucia.guillamet@hotmail.com>
2024-03-27 08:44:20 +0100

    evaluation metrics . added dependencies to environment

--- src/evaluationmetrics.py
+++ src/evaluationmetrics.py
@@ -1,61 +1,111 @@
-def rouge(y_true: str, y_pred: str) -> float:
-    """Rouge loss function to be applied element wise.
-
-    Args:
-        y_true (str): True value.
-        y_pred (str): Predicted value.
-
-    Returns:
-        float: Loss value.
-    """
-    # TODO: Implement rouge loss
-    # Not sure if inputs should be strings or tokenised lists?? Haven't looked at this at all
-    # presumably there's a library for this?
-    return None
+import evaluate
+from bert_score import BERTScorer
+import numpy as np
+from typing import List, Callable
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def other_loss_metric(y_true: str, y_pred: str) -> float:
-    # TODO
-    pass
+class ModelEvaluator:
+    # Class to evaluate models. Includes Rouge, Bleu and Bert score.
+    # Input is a pair of lists of the generated summaries and the reference summaries.
+    def __init__(self):
+        self.BERTscorer = BERTScorer(model_type='bert-base-uncased')
+        self.rouge_scorer = evaluate.load('rouge')
 
+    def rouge_score(self, y_pred, y_true):
+    
+        """Rouge loss function.
 
-def eval_loss(y_true: list, y_pred: list, loss_metric="rouge") -> list:
-    """Apply chosen loss metric to each element in the batch
+        Args:
+            y_true (list): True value.
+            y_pred (list): Predicted value.
 
-    Args:
-        y_true (list): List of true values.
-        y_pred (list): List of predicted values.
+        Returns:
+            f1_score for rouge 1, 2 and : float
+        """
+        scorer = self.rouge_scorer
+        scores = scorer.compute(references = y_true, predictions  =y_pred, use_stemmer=True, use_aggregator=False)
+        return scores
+    def BERTscore(self, y_pred, y_true):
+        """BERT loss function.
+:
+        Args:
+            y_true (list): True value.
+            y_pred (list): Predicted value.
 
-    Returns:
-        list: List of losses for each element in the batch.
-    """
-    # TODO: Vectorize this (if possible)
-    LOSS_FUNCS = {"rouge": rouge, "other": other_loss_metric}
-    assert len(y_true) == len(y_pred), "y_true and y_pred must have the same length"
-    return [LOSS_FUNCS[loss_metric](y_true[i], y_pred[i]) for i in range(len(y_true))]
+        Returns:
+            F1: float.
+        """
+        scorer = self.BERTscorer    
+        _, _, F1 = scorer.score(y_true, y_pred)
+        return  F1
+    
+    def batchify(self, data: List, batch_size: int):
+        """Split a list into batches."""
+        return [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
 
+    def evaluation(self, y_true: List[str], y_pred: List[str], loss_func: Callable, batch_size: int = 100) -> List:
+        """Apply chosen loss metric to each batch of elements.
 
-def test_rouge():
-    """
-    Test the rouge function.
+        Args:
+            y_true (List[str]): List of true values.
+            y_pred (List[str]): List of predicted values.
+            loss_func (Callable): Loss function to apply.
+            batch_size (int, optional): Batch size. Defaults to 32.
 
-    This function tests the rouge function by comparing the output of the function
-    with expected values. It asserts that the calculated rouge score matches the
-    expected score for different input strings.
+        Returns:
+            List: List of losses for each batch.
+        """
+        assert len(y_true) == len(y_pred), "y_true and y_pred must have the same length"
+        
+        # Split data into batches
+        y_true_batches = self.batchify(y_true, batch_size)
+        y_pred_batches = self.batchify(y_pred, batch_size)
+        
+          # Apply loss function to each pair in each batch
+        losses = []
+        for y_true_batch, y_pred_batch in zip(y_true_batches, y_pred_batches):
+            batch_losses = [loss_func(y_true_batch, y_pred_batch)]
+            losses.extend(batch_losses)
+        
+        # # Calculate mean and variance of scores
+        if loss_func == self.rouge_score:
+            rouge1_vals = [d['rouge1'] for d in losses]
+            rouge2_vals = [d['rouge2'] for d in losses]
+            rougeL_vals = [d['rougeL'] for d in losses]
+            #rougeLsum_vals = [d['rougeLsum'][0] for d in losses]
+            mean_rouge1 = np.mean(rouge1_vals)
+            var_rouge1 = np.var(rouge1_vals)
+            mean_rouge2 = np.mean(rouge2_vals)
+            var_rouge2 = np.var(rouge2_vals)
+            mean_rougeL = np.mean(rougeL_vals)
+            var_rougeL = np.var(rougeL_vals)
+            return rouge1_vals, rouge2_vals, rougeL_vals, mean_rouge1, var_rouge1, mean_rouge2, var_rouge2, mean_rougeL, var_rougeL
 
-    Returns:
-        None
-    """
-    y_true = "This is a test"
-    y_pred = "This is a test"
-    assert rouge(y_true, y_pred) == 1.0  # guessing this is the expected value?
-    y_pred = "This is not a test"
-    assert rouge(y_true, y_pred) == 0.0
-    y_pred = ""
+        else:
+            mean_loss = np.mean(losses)
+            var_loss = np.var(losses)
+            return losses, mean_loss, var_loss
+    def plot_scores(self, score_values, score_name):
+        """
+        Plots a histogram and boxplot for the given scores.
 
-    y_list = ["This is a test", "This is a test", "This is a test"]
-    assert eval_loss(y_list, y_list) == [1.0, 1.0, 1.0]
+        Args:
+        scores (list): The list of scores to plot.
+        score_name (str): The name of the score (for the plot title).
+        """
+        # Plot histogram
+        plt.figure(figsize=(10, 4))
 
+        plt.subplot(1, 2, 1)
+        plt.hist(score_values, bins=20, color='blue', alpha=0.7)
+        plt.title(f'Histogram of {score_name} Scores')
 
-if __name__ == "__main__":
-    test_rouge()
+        # Plot boxplot
+        plt.subplot(1, 2, 2)
+        sns.boxplot(score_values, color='green')
+        plt.title(f'Boxplot of {score_name} Scores')
+
+        plt.tight_layout()
+        plt.show()
