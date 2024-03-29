2972b615bbfc56f7a11b392ac77dfef1c8f3d00d
Isaac Watson <ucabiiw@ucl.ac.uk>
2024-03-29 12:22:07 +0000

    AI eval fix

--- src/inference.py
+++ src/inference.py
@@ -2,6 +2,12 @@ from transformers import GenerationConfig
 from tqdm import tqdm
 import torch
 from abc import ABC
+import numpy as np
+
+from dotenv import load_dotenv
+
+load_dotenv()
+cohere_api_key = os.getenv("COHERE_PROD_API_KEY")
 
 class Inference():
 
@@ -53,56 +59,72 @@ class Inference():
 
         return posts, model_summaries, true_summaries
     
-    def _ai_rank_summaries(full, true, pred):
 
-        import re
-        import cohere
+def _ai_rank_summaries(full, true, pred):
 
-        co = cohere.Client(cohere_key)
+    import re
+    import cohere
 
-        format = f"""### FULL TEXT:\n {full}
-        ### SUMMARY 1:\n{true}\n\n
-        ### SUMMARY 2:\n{pred}"""
+    co = cohere.Client(cohere_key)
 
-        response = (
-            co.generate(
-                model="command-nightly",
-                # Can this prompt be tailored to cohere's command-nightly model?
-                prompt=f"""You are an expert in text summarization. Below, you are given two summaries.
+    format = f"""### FULL TEXT:\n {full}
+    ### SUMMARY 1:\n{true}\n\n
+    ### SUMMARY 2:\n{pred}"""
 
-            {format}
+    response = (
+        co.generate(
+            model="command-nightly",
+            # Can this prompt be tailored to cohere's command-nightly model?
+            prompt=f"""You are an expert in text summarization. Below, you are given two summaries.
 
-            Your role is to determine the best of the two summaries provided.
-            Return 1 if you think summary 1 is the best.
-            Return 2 if you think summary 2 is the best.
+        {format}
 
-            Use the following chain-of-thought reasoning to evaluate each summary:
-            1. Does the summary accurately represent the full text?
-            2. Is the summary factually correct?
-            3. Is the summary coherent and easy to understand?
+        Your role is to determine the best of the two summaries provided.
+        Return 1 if you think summary 1 is the best.
+        Return 2 if you think summary 2 is the best.
 
-            Your response should only be an integer number that represents the best summary.
-            """,
-                max_tokens=5,
-                temperature=0.2,
-            )
-            .generations[0]
-            .text
+        Use the following chain-of-thought reasoning to evaluate each summary:
+        1. Does the summary accurately represent the full text?
+        2. Is the summary factually correct?
+        3. Is the summary coherent and easy to understand?
+
+        Ideally, you must chose one of the two summaries. 
+        However, if you think both summaries are equally good and you cannot make a decision, return 0.
+
+        You must only return 0, 1, or 2. Do not return any text or additional information.
+        """,
+            max_tokens=5,
+            temperature=0.2,
+            seed=42,
         )
-        score = int(re.findall(r"[-+]?(?:\d*\.*\d+)", response)[0])
+        .generations[0]
+        .text
+    )
 
-        return score
-    
+    return response
 
-    def win_rate(self, full_texts, summaries_1, summaries_2, n_samples=100):
-        results = []
-        i = 0
-        for full, sum_1, sum_2 in zip(full_texts, summaries_1, summaries_1):
+def win_rate(full_texts, summaries_1, summaries_2, n_samples=100):
+    results = []
+    i = 0
+    for full, sum_1, sum_2 in zip(full_texts, summaries_1, summaries_1):
+        # randomly permute the summaries; avoid bias
+        p = np.random.permutation([1, 0])
+        sum_1, sum_2 = [sum_1, sum_2][p[0]], [sum_1, sum_2][p[1]]
         if i >= n_samples:
             break
-        result = self._ai_rank_summaries(full, sum_1, sum_2)
-        results.append(result)
+        result = _ai_rank_summaries(full, sum_1, sum_2)
+        result = int(result)
+        if p[0] == 1 and result == 1:
+            results.append(2)
+        elif p[0] == 1 and result == 2:
+            results.append(1)
+        else:
+            results.append(result)
         i += 1
-        win_rate_1 = results.count(1) / len(results)
-        win_rate_2 = results.count(2) / len(results)
-        return results, win_rate_1, win_rate_2
+    win_rate_1 = results.count(1) / len(results)
+    win_rate_2 = results.count(2) / len(results)
+    if 0 in results:
+        draw_rate = results.count(0) / len(results)
+    else:
+        draw_rate = 0
+    return results, win_rate_1, win_rate_2, draw_rate
